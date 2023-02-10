b4318b530570e3b45ab30dc3aa0949a0bb168dd5
saocdr <111291256+saocdr@users.noreply.github.com>
2023-02-10 19:17:10 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -47,8 +47,8 @@ svn export https://github.com/coolsnowwolf/lede/tree/master/package/lean/default
 # 添加UPX UCL工具包
 # cp -r lede/tools/upx tools
 # cp -r lede/tools/ucl tools
-mkdir -p tools/ucl && wget -P tools/ucl https://github.com/coolsnowwolf/lede/tree/master/package/lean/ucl/Makefile
-mkdir -p tools/upx && wget -P tools/upxh ttps://github.com/coolsnowwolf/lede/tree/master/package/lean/upx/Makefile
+# mkdir -p tools/ucl && wget -P tools/ucl https://github.com/coolsnowwolf/lede/tree/master/package/lean/ucl/Makefile
+# mkdir -p tools/upx && wget -P tools/upxh ttps://github.com/coolsnowwolf/lede/tree/master/package/lean/upx/Makefile
 
 # 修改makefile
 sed  -i '/^# builddir dependencies/i\tools-y += ucl upx' ./tools/Makefile
